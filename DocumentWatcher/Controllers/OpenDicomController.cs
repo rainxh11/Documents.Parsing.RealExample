6ec2bcf5e00ea9d96a182fb9cef2b002476a32cd
rainxh11 <rainxh11@gmail.com>
2022-05-13 17:09:05 +0100

    +first commit

---
+++ DocumentWatcher/Controllers/OpenDicomController.cs
@@ -0,0 +1,64 @@
+﻿using DocumentWatcher.Helpers;
+using DocumentWatcher.Models;
+using EmbedIO;
+using EmbedIO.Routing;
+using EmbedIO.WebApi;
+using MongoDB.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentWatcher.Controllers
+{
+    public class OpenDicomController : WebApiController
+    {
+        [Route(HttpVerbs.Post, "/opendicom/{id}")]
+        public async Task<string> OpenDicom(string id)
+        {
+            try
+            {
+                var config = ConfigHelper.GetConfig();
+                var response = await Program.dicomServerApi.DownloadStudyArchive(id);
+                var archive = response.Content.ReadAsStream();
+
+                var destinationDir = new DirectoryInfo(config.DicomTemp);
+                if(!destinationDir.Exists) destinationDir.Create();
+                var file = new FileInfo(destinationDir.FullName + $@"\{id}.zip");
+
+                if (!file.Exists)
+                {
+                    using (var fileStream = new FileStream(file.FullName, FileMode.CreateNew))
+                    {
+                        byte[] buffer = new byte[8 * 1024];
+                        int len;
+                        while((len = archive.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            fileStream.Write(buffer, 0, len);
+                        }
+                        fileStream.Close();    
+                    }
+                }
+                var dicomViewer = DicomViewerHelper.GetDicomViewer(config.DicomViewer);
+                var viewer = new Process()
+                {
+                    StartInfo = new ProcessStartInfo()
+                    {
+                        FileName = file.FullName,
+                        Arguments = $"\"{file.FullName}\""
+                    }
+                };
+                viewer.Start();
+
+                return $"Success, Opened Study: {id}";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
---
+++ DocumentWatcher/Helpers/FileWatcher.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Globalization;
+using System.Threading;
+
+namespace DocumentWatcher.Helpers
+{
+    public class FileWatcher
+    {
+        public static FileSystemWatcher DocumentWatcher;
+        public static IObservable<string> ChangingFiles;
+
+        public static void Init()
+        {
+            var config = ConfigHelper.GetConfig();
+
+            var documentFolder = new DirectoryInfo(config.DocumentFolder);
+            if (!documentFolder.Exists)
+            {
+                documentFolder.Create();
+            }
+
+            DocumentWatcher = new FileSystemWatcher(config.DocumentFolder, config.ExtensionFilter);
+            DocumentWatcher.IncludeSubdirectories = true;
+
+            DocumentWatcher.NotifyFilter = NotifyFilters.LastAccess
+                | NotifyFilters.LastWrite
+                | NotifyFilters.Size
+                | NotifyFilters.FileName
+                | NotifyFilters.DirectoryName
+                | NotifyFilters.CreationTime;
+
+            DocumentWatcher.EnableRaisingEvents = true;
+
+            ChangingFiles = Observable
+                .FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>(
+                    h => DocumentWatcher.Changed += h,
+                    h => DocumentWatcher.Changed -= h)
+                .Where(x => x.EventArgs.ChangeType != WatcherChangeTypes.Created && x.EventArgs.ChangeType == WatcherChangeTypes.Changed)
+                .Select(x => x.EventArgs.FullPath);
+
+
+        }
+
+        private static void ArchiveDocument(FileInfo file)
+        {
+            try
+            {
+                var dir = file.Directory.FullName + $@"\ARCHIVE\{file.CreationTime.ToString("yyyy-MM (MMMM)", CultureInfo.CreateSpecificCulture("fr-FR"))}\{file.CreationTime.ToString("yyyy-MM-dd (dd MMMM yyyy)", CultureInfo.CreateSpecificCulture("fr-FR"))}".ToUpperInvariant();
+
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                File.Move(file.FullName, dir + $@"\{file.Name}");
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static (bool, FileInfo) IsFileLocked(FileInfo file)
+        {
+            try
+            {
+                using (FileStream stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    stream.Close();
+                }
+            }
+            catch (IOException)
+            {
+                return (true, file);
+            }
+
+            return (false, file);
+        }
+
+        public static void Start()
+        {
+            var config = ConfigHelper.GetConfig();
+            var semaphore = new SemaphoreSlim(config.InitialConcurrentRefreshs, config.MaxConcurrentRefreshs);
+
+            FileWatcher.ChangingFiles
+                .Where(x => !x.Contains("$"))
+                .Delay(TimeSpan.FromSeconds(5))
+                .Subscribe(
+                    async x =>
+                    {
+                        var file = new FileInfo(x);
+
+                        try
+                        {
+                            await Task.Run(async () =>
+                            {
+                                while (IsFileLocked(file).Item1)
+                                {
+                                    //Console.WriteLine($"File '{file.Name}' is being used by another process, retyring in 5 seconds.");
+                                    await Task.Delay(TimeSpan.FromSeconds(5));
+                                }
+                            });
+                            //Console.WriteLine($"Saving changes of file '{file.Name}'");
+                            await semaphore.WaitAsync();
+                            await DocumentHelper.UpdateStudy(file);
+                            semaphore.Release();
+                        }
+                        catch (Exception ex)
+                        {
+                            semaphore.Release();
+                            //Console.WriteLine($"Error in saving file '{file.Name}', Exception Message: {Environment.NewLine}'{ex.Message}'");
+                        }
+                    }
+                );
+
+            Observable
+                .Interval(TimeSpan.FromSeconds(10))
+                .Repeat()
+                .SelectMany(x => new DirectoryInfo(config.DocumentFolder).GetFiles("*.docx"))
+                .Where(x => DateTime.Now > x.CreationTime && (DateTime.Now - x.CreationTime) >= new TimeSpan(24, 0, 0))
+                .Where(x => !IsFileLocked(x).Item1)
+                .Where(x => (DateTime.Now - x.LastAccessTime).Hours >= 1)
+                .Do(async x => 
+                {
+                    await semaphore.WaitAsync();
+                    ArchiveDocument(x);
+                    semaphore.Release();
+                })
+                .Subscribe();
+
+            Observable
+                .Interval(TimeSpan.FromSeconds(5))
+                .Repeat()
+                .SelectMany(x => new DirectoryInfo(config.DocumentFolder).GetFiles("*.docx"))
+                .Select(x => IsFileLocked(x))
+                .Do(async x => 
+                {
+                    await semaphore.WaitAsync();
+                    await DocumentHelper.ToggleStudy(x.Item2, x.Item1 ? "inProgress" : "complete");
+                    semaphore.Release();
+
+                })
+                  .Subscribe();
+
+            Observable
+                .Interval(TimeSpan.FromSeconds(5))
+                .Repeat()
+                .SelectMany(x => new DirectoryInfo(config.DocumentFolder).GetFiles("*.docx", SearchOption.AllDirectories))
+                .Where(x => !IsFileLocked(x).Item1)
+                .Do(async x => 
+                {
+                    await semaphore.WaitAsync();
+                    await DocumentHelper.CatchupStudy(x);
+                    semaphore.Release();
+                })
+                .Subscribe();
+
+        }
+    }
+}
